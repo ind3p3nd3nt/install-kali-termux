ef15103b50e69208014e9f4219f7b6022b340761
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-08 11:55:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -313,10 +313,9 @@ function fix_profile_bash() {
 
 function fix_sudo() {
     ## fix sudo & su on start
-    chmod +s $CHROOT/usr/bin/sudo
-    chmod +s $CHROOT/usr/bin/su
-	echo "$USERNAME    ALL=(ALL:ALL) NOPASSWD:ALL" > $CHROOT/etc/sudoers
-
+   if [ -f "$CHROOT/usr/bin/sudo" ]; then chmod +s $CHROOT/usr/bin/sudo; fi
+   if [ -f "$CHROOT/usr/bin/su" ]; then chmod +s $CHROOT/usr/bin/su; fi
+   if [ -f "$CHROOT/etc/sudoers" ]; then echo "$USERNAME    ALL=(ALL:ALL) NOPASSWD:ALL" > $CHROOT/etc/sudoers; fi
     # https://bugzilla.redhat.com/show_bug.cgi?id=1773148
     echo "Set disable_coredump false" > $CHROOT/etc/sudo.conf
 }
