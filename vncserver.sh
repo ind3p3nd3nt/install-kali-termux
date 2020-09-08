e94b3fb5ba0db118f5f5392001df489193396666
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-08 18:10:56 -0400

    Update vncserver.sh

--- vncserver.sh
+++ vncserver.sh
@@ -4,7 +4,7 @@ echo 'lxsession &' >~/.vnc/xstartup;
 echo 'lxterminal &' >>~/.vnc/xstartup;
 apt update && apt install tigervnc-standalone-server lxde-core net-tools lxterminal -y;
 myip=$(ifconfig wlan0 | grep inet)
-rm -rf /tmp/.X*;
+rm -rf /tmp/.X3-lock;
 vncserver -kill :3;
 vncserver :3 -localhost no;
 echo 'VNC Server listening on 0.0.0.0:5903 you can remotely connect another device to that display with a vnc viewer';
