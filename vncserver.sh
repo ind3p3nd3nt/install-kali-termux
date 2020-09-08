9217b54e652c0178b68c7f9522de275bbc5d33c8
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-08 12:10:59 -0700

    Create vncserver.sh

---
+++ vncserver.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+mkdir ~/.vnc;
+echo 'lxsession &' >~/.vnc/xstartup;
+echo 'lxterminal &' >>~/.vnc/xstartup;
+apt update && apt install tigervnc-standalone-server lxde-core net-tools lxterminal -y;
+myip=$(ifconfig wlan0 | grep inet)
+vncserver :3 -localhost no;
+echo 'VNC Server listening on 0.0.0.0:5903 you can remotely connect another device to that display with a vnc viewer';
+echo "Your Phone IP address: $myip";
