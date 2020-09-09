70367331b19e2ba18461fd4bb51f15c23eead196
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-09 01:09:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -338,7 +338,7 @@ function fix_sudo() {
    chmod +s $CHROOT/usr/bin/sudo 
    fi
    if [ ! -f "$CHROOT/usr/bin/su" ]; then chmod +s $CHROOT/usr/bin/su; fi
-   if [ ! -f "$CHROOT/etc/sudoers.d/${USERNAME}" ]; then echo "${USERNAME}    ALL=(ALL:ALL) NOPASSWD:ALL" > $CHROOT/etc/sudoers.d/${USERNAME} && echo "root    ALL=(ALL:ALL) ALL" > $CHROOT/etc/sudoers; fi
+   if [ ! -f "$CHROOT/etc/sudoers.d/${USERNAME}" ]; then echo "${USERNAME}    ALL=(ALL:ALL) NOPASSWD:ALL" > $CHROOT/etc/sudoers.d/${USERNAME}; fi
     # https://bugzilla.redhat.com/show_bug.cgi?id=1773148
     echo "Set disable_coredump false" > $CHROOT/etc/sudo.conf
 }
