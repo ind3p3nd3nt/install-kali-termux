d701ed536c8c78fe9edba80c6e1c2306e9b06856
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-09 02:08:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -191,8 +191,8 @@ fi
 if [[ \$KALIUSR == "0" || ("\$#" != "0" && ("\$1" == "-r" || "\$1" == "-R")) ]];then
     user="root"
     home="/\$user"
-    char='"'
-    cmd1="$charapt update && apt install busybox sudo -y$char"
+    char="'"
+    cmd1="${char}apt update && apt install busybox sudo -y${char}"
     start="/bin/bash --login -c $cmd1" 
     if [[ "\$#" != "0" && ("\$1" == "-r" || "\$1" == "-R") ]];then
         shift
