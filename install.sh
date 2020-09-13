82888b0a9d6ae841423218081bc3bb83c46f2cc6
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2020-09-13 03:26:00 -0400

    fix remote stop

--- install.sh
+++ install.sh
@@ -267,16 +267,17 @@ EOF
 function remote() {
     NH_REMOTE=${PREFIX}/bin/remote
     cat > $NH_REMOTE <<- EOF
-#!/data/data/com.termux/files/usr/bin/bash -e
+#!/bin/bash -e
 cd \${HOME}
 unset LD_PRELOAD
 if [ "\$1" = "install" ]; then
 	nh -r apt update && nh -r apt install tigervnc-standalone-server lxde-core kali-menu net-tools lxterminal -y;
 fi
 if [ "\$1" = "stop" ]; then
-	if [ -f "\$CHROOT/tmp/.X3-lock" ]; then rm -rf \$CHROOT/tmp/.X3-lock && nh -r /bin/vncserver -kill :3; fi
+	if [ -f "$CHROOT/tmp/.X3-lock" ]; then rm -rf $CHROOT/tmp/.X3-lock && nh -r /bin/vncserver -kill :3; fi
 fi
 if [ "\$1" = "start" ]; then
+	if [ -f "$CHROOT/tmp/.X3-lock" ]; then rm -rf $CHROOT/tmp/.X3-lock && nh -r /bin/vncserver -kill :3; fi
 	echo 'VNC Server listening on 0.0.0.0:5903 you can remotely connect another device to that display with a vnc viewer';
 	myip=\$(ifconfig | grep inet) 
 	echo "\$myip";
@@ -289,6 +290,7 @@ exit 0
 EOF
     chmod +x $NH_REMOTE  
 }
+
 
 
 
