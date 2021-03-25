941c5f20731c560820df70a940c9961da75ab592
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-03-24 20:58:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -198,9 +198,10 @@ function update() {
 unset LD_PRELOAD
 user="root"
 home="/root"
+cmd0="wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2020.2_all.deb && apt install ./kali-archive-keyring_2020.2_all.deb"
 cmd1="apt update"
 cmd2="apt-get install busybox sudo kali-menu kali-tools. -y"
-cmd3="apt full-upgrade -y"
+cmd3="apt dist-upgrade -y"
 cmd4="apt auto-remove -y"
 nh -r \$cmd1;
 nh -r \$cmd2;
